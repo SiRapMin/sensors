13b30ca83021fe4daf447bae82eb307047e91ecf
SiRapMin <56983070+SiRapMin@users.noreply.github.com>
2019-11-13 18:33:38 -0500

    Update window.py
    Send com

--- Main Files/window.py
+++ Main Files/window.py
@@ -1,6 +1,9 @@
 from PyQt5.QtWidgets import QApplication,QMainWindow,QMessageBox
 from PyQt5 import uic, QtCore, Qt
 from main import *
+import sys
+import glob
+import serial
 
 class Window(QMainWindow):
 	
@@ -10,13 +13,45 @@ class Window(QMainWindow):
 		QMainWindow.__init__(self)
 		#load file
 		uic.loadUi("window.ui",self)
-		
+		self.SerialPort = "COM3"
 		self.initUI()
 
 
 	def initUI(self):
-		self.collect.clicked.connect(lambda: WindowPygame())
+		print(self.serial_ports())
+		for puerto in self.serial_ports():
+			self.opcionSerial.addItem(puerto)
+		self.collect.clicked.connect(lambda: WindowPygame(self.opcionSerial.currentText()))
 		#self.consult.clicked.connect(print('consult'))
+
+	def serial_ports(self):
+		""" Lists serial port names
+
+            :raises EnvironmentError:
+                On unsupported or unknown platforms
+            :returns:
+                A list of the serial ports available on the system
+        """
+		if sys.platform.startswith('win'):
+			ports = ['COM%s' % (i + 1) for i in range(256)]
+		elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+			# this excludes your current terminal "/dev/tty"
+			ports = glob.glob('/dev/tty[A-Za-z]*')
+		elif sys.platform.startswith('darwin'):
+			ports = glob.glob('/dev/tty.*')
+		else:
+			raise EnvironmentError('Unsupported platform')
+
+		result = []
+		for port in ports:
+			try:
+				s = serial.Serial(port)
+				s.close()
+				result.append(port)
+			except (OSError, serial.SerialException):
+				pass
+		return result
+
 
 #start application
 app=QApplication(sys.argv)
